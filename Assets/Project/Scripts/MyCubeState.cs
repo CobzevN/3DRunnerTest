726f77fde5c450a19875ce45e3737544c171c164
CobzevN <104561279+CobzevN@users.noreply.github.com>
2022-04-28 12:51:35 +0300

    Add files via upload

---
+++ Assets/Project/Scripts/MyCubeState.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyCubeState : MonoBehaviour
+{
+    public GameObject myCube, takingCollision;
+    public int heigh;
+    public static bool dead;
+
+    void Update()
+    {
+        myCube.transform.position = new Vector3(transform.position.x, heigh + 1, transform.position.z); //повышает высоту MyCube при взятии
+        takingCollision.transform.localPosition = new Vector3(0, -heigh, 0); //снижает коллизию поднятия по высоте "Taking Collision", так как после поднятия куба она повышается
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Takable" && other.gameObject.GetComponent<TakeCube>().GetTakeState() == false) //&& проверяет если у меня уже был взят куб и возвращает false 
+        {
+            heigh += 1;
+            other.gameObject.GetComponent<TakeCube>().Take(); //Говорит классу TakeCube что куб был взят
+            other.gameObject.GetComponent<TakeCube>().TakeIndex(heigh); //Говорит классу TakeCube, что TakeIndex должен добавить значение heigh
+            other.gameObject.transform.parent = myCube.transform; //Делает иерархию в персонаже MyCybe, перемещая в него новый, поднятый объект с тэгом Takable
+        }
+        if (other.gameObject.tag == "Wall")
+        {
+            if (heigh <= 0)
+            {
+                print("DEAD WALL COLLISION MyCubeState");
+                dead = true;
+            }
+        }
+    }
+}
